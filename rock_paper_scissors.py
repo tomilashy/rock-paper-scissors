05c7cf5f445ac7dcf2bce6102cb142b22b902703
Olasubulumi Ogo-Oluwa Jesutomi <33330651+tomilashy@users.noreply.github.com>
2018-05-05 09:15:57 -0400

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -5,6 +5,8 @@ Created on May 2, 2018
 '''
 import random
 exits="no";
+player1=0;
+player2=0;
 while exits !="yes":
     user1=input("Enter player 1's choice: ")
 
@@ -17,22 +19,34 @@ while exits !="yes":
                 print("Its a draw")
             elif user1=="rock":
                 if user2=="paper":
-                    print("player 2 wins")
+                    print("player 2 wins");player2+=1
+                
                 else:
-                    print("player 1 wins")
+                    print("player 1 wins");player1+=1
             elif user1=="paper":
                 if user2=="scissors":
-                    print("player 2 wins")
+                    print("player 2 wins");player2+=1
                 else:
-                    print("player 1 wins")
+                    print("player 1 wins");player1+=1
             elif user1=="scissors":
                 if user2=="rock":
-                    print("player 2 wins")
+                    print("player 2 wins");player2+=1
                 else:
-                    print("player 1 wins")
+                    print("player 1 wins");player1+=1
         else: 
             print("something went wrong")
     else: 
         print("something went wrong")
-
+    
     exits=input("do you want to exit? input \"yes\" or \"no\" ")
+print(f"player 1 :{player1} \ncomputer :{player2}")    
+    
+
+if player1>player2:
+    print("player 1 wins")
+elif player1 == player2:
+     print("Its a tie")
+else:
+     print("player 2 wins")
+     
+
